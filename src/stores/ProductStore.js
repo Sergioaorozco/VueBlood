18a26f9b1e726f8d0433ec69637875638755b553
Sergio <sergioa.orozcoo@gmail.com>
2023-02-08 14:25:18 -0500

    Change for testing the api used to display products

--- src/stores/ProductStore.js
+++ src/stores/ProductStore.js
@@ -1,4 +1,5 @@
 import { defineStore } from 'pinia';
+import axios from 'axios';
 // Products in Our app
 
 export const useProductStore = defineStore('productStore', {
@@ -10,9 +11,25 @@ export const useProductStore = defineStore('productStore', {
     },
   actions: {
     async pullProducts() {
-      const response = await fetch('https://fakestoreapi.com/products')
-      let items = await response.json();
-      return this.products.push(...items);
+    //   const response = await fetch('https://fakestoreapi.com/products?limit=10')
+    //   let items = await response.json();
+    //   return this.products.push(...items);
+    
+    const options = {
+      method: 'GET',
+      url: 'https://amazon-product-reviews-keywords.p.rapidapi.com/product/search',
+      params: {keyword: 'iphone', country: 'US', category: 'aps'},
+      headers: {
+        'X-RapidAPI-Key': 'bc2a1b6975mshf963868f2f09482p183802jsn59306543f071',
+        'X-RapidAPI-Host': 'amazon-product-reviews-keywords.p.rapidapi.com'
+      }
+    };
+
+      axios.request(options).then(function (response) {
+        console.log(response.data);
+      }).catch(function (error) {
+        console.error(error);
+      });
     }
   }
   // Action
