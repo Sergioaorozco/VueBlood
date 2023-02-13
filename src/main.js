f2d8981e4720c131ad8e50ec59450dc08132d887
Sergio <sergioa.orozcoo@gmail.com>
2023-02-13 15:45:35 -0500

    Add Environment Variables to test

--- src/main.js
+++ src/main.js
@@ -6,18 +6,6 @@ import { initializeApp,  } from "firebase/app";
 import App from './App.vue';
 import PrimeVue from 'primevue/config';
 
-// SDK Information
-// const firebaseConfig = {
-//   apiKey: "AIzaSyC4PG0KkCwepCOOAJSB-XqXnRyPibj9978",
-//   authDomain: "vueshop-3dd1a.firebaseapp.com",
-//   databaseURL: "https://vueshop-3dd1a-default-rtdb.firebaseio.com",
-//   projectId: "vueshop-3dd1a",
-//   storageBucket: "vueshop-3dd1a.appspot.com",
-//   messagingSenderId: "916696338688",
-//   appId: "1:916696338688:web:158f5e4c50c238457023e5",
-//   measurementId: "G-PYYZ0KCBEY"
-// };
-
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_VUE_KEY,
   authDomain: import.meta.env.VITE_VUE_AUTH_DOMAIN,
@@ -28,7 +16,8 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_VUE_APP_ID,
   measurementId: import.meta.env.VITE_VUE_MEASUREMENT_ID
 };
-console.log(import.meta.env.VITE_VUE_AUTH_DOMAIN)
+
+
 
 // Importing Styles
 import './style.css'
@@ -38,6 +27,7 @@ import 'primeicons/primeicons.css'
 
 // Initialize PrimeVue, Firebase and Vue
 const app = initializeApp(firebaseConfig);
+console.log(app)
 
 const pinia = createPinia();
 const vue = createApp(App);
